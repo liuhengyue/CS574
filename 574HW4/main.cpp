256e62b629d518e83cb51d58e986671ef495726b
liuhengyue <6hengyue@gmail.com>
2015-11-08 03:06:05 -0800

    Initial Commit

---
+++ 574HW4/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  574HW4
+//
+//  Created by LiuHengyue on 11/8/15.
+//  Copyright © 2015 LiuHengyue. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
